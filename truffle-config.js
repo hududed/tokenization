7a50f5b3079b12e59abfba3f759eb5e0c2e06263
hud <hudwahab@gmail.com>
2021-06-27 15:17:28 -0600

    unit tests OK

--- truffle-config.js
+++ truffle-config.js
@@ -5,13 +5,15 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      port: 7545,
+      network_id: "*",
+      host: "127.0.0.1"
     }
   },
   compilers: {
     solc: {
-      version: "0.6.2"
+      version: "^0.6.0"
     }
   }
 };
