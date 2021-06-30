8cac2509944c47871bfb86d2da2c0bb26a20e363
hud <hudwahab@gmail.com>
2021-06-30 15:54:25 -0600

    multi truffle test, 1 chai setup OK

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -1,31 +1,23 @@
 const Token = artifacts.require("MyToken");
 
-var chai = require("chai");
-
+const chai = require("./chaisetup.js");
 const BN = web3.utils.BN;
-const chaiBN = require('chai-bn')(BN);
-chai.use(chaiBN);
-
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-
 const expect = chai.expect;
+
+require('dotenv').config({ path: '../.env' });
 
 contract("Token Test", function (accounts) {
     const [initialHolder, recipient, anotherAccount] = accounts;
 
     beforeEach(async () => {
-        this.myToken = await Token.new(1000);
+        this.myToken = await Token.new(process.env.INITIAL_TOKENS);
     });
 
     it("All tokens should be in my account", async () => {
         let instance = this.myToken;
         let totalSupply = await instance.totalSupply();
-        //old style:
-        //let balance = await instance.balanceOf.call(initialHolder);
-        //assert.equal(balance.valueOf(), 0, "Account 1 has a balance");
-        //condensed, easier readable style:
-        await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+        // await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+        return expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
     });
 
     it("I can send tokens from Account 1 to Account 2", async () => {
@@ -35,7 +27,8 @@ contract("Token Test", function (accounts) {
         await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
         await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
         await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
-        await expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+        // await expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+        return expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
     });
 
 
@@ -46,7 +39,7 @@ contract("Token Test", function (accounts) {
         await expect(instance.transfer(recipient, new BN(balanceOfAccount + 1))).to.eventually.be.rejected;
 
         //check if the balance is still the same
-        await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balanceOfAccount);
+        return expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balanceOfAccount);
 
     });
 
